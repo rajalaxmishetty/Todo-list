8a7511310b9d49894874a96e0cf26c89cf120c80
rajalaxmishetty <rajishetty699@gmail.com>
2022-07-14 10:43:41 +0530

    first commit

---
+++ src/Mycomponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className="text-center">
+      This is an about component
+      <p style={{color:"blue"}}>This is application where we can use it as Todo list which helps easy to note down the things</p>
+    </div>
+  )
+}
+
+
+
---
+++ src/Mycomponents/Todos.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Todoitem} from "./Todoitem";
+
+
+export const Todos = (props) => {
+    let myStyle = {
+     
+      minHeight:"70vh",
+      margin: "40px auto"
+    }
+
+  return (
+    <div className="container " style={myStyle}>
+      <h3 className="my-3">Todos list</h3>
+      {props.todos.length===0? "no todos to display":
+       props.todos.map((todo)=>{
+        return (
+         <Todoitem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+        )
+      })
+      }
+      
+    </div>
+  )
+}
+//using hr inside return the first use open bracket then <> this
+
